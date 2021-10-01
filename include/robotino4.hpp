56daa186a4436a7c1e8b85f42aa3234f427a4e2a
BrOleg5 <oleg.brylyov@mail.ru>
2021-10-01 13:42:41 +0700

    Add Doxygen style code documentation in header. Delete documentation from source file.

--- include/robotino4.hpp
+++ include/robotino4.hpp
@@ -9,45 +9,224 @@ using namespace rec::robotino::api2;
 #   define PI 3.14159265358979323846
 #endif
 
+/**
+ * @class MyCom
+ * @brief Implementation of Com() class.
+ */
 class MyCom : public Com
 {
 public:
 	MyCom() : Com() {}
 
+    /**
+     * This function is called on errors.
+     * 
+     * @param errorString a human readable error description.
+     * @throws noting
+     * @remark This function is called from the thread in which Com::processEvents() is called.
+     */
 	void errorEvent( const char* errorString );
+
+    /**
+     * This function is called if a connection to Robotino has been established.
+     * 
+     * @throws nothing
+     * @remark This function is called from outside the applications main thread. 
+     * This is extremely important particularly with regard to GUI applications.
+     * This function is called from the thread in which Com::processEvents() is called.
+     */
 	void connectedEvent();
+
+    /**
+     * This function is called when a connection is closed.
+     * 
+     * @throws nothing
+     * @remark This function is called from outside the applications main thread.
+     * This is extremely important particularly with regard to GUI applications.
+     * This function is called from the thread in which Com::processEvents() is called.
+     */
 	void connectionClosedEvent();
+
+    /**
+     * This function is called when a log message is posted.
+     * 
+     * @param message the log message.
+     * @param level the log level.
+     * @throws nothing.
+     * @remark This function is called from outside the applications main thread.
+     * This is extremely important particularly with regard to GUI applications.
+     * This function is called from the thread in which Com::processEvents() is called.
+     */
 	void logEvent( const char* message, int level );
+
+    /**
+     * This function is called when use ping to robot.
+     * 
+     * @param timeMs ping value.
+     */
 	void pingEvent( float timeMs );
 };
 
+/**
+ * @class Robotino4
+ * @brief Robotino API2 wrapper.
+ */
 class Robotino4
 {
 private:
-    size_t motor_num = 3;
+
+    /**
+     * Number of Robotino motors.
+     */
+    const size_t motor_num = 3;
+
+    /**
+     * Robotino comunication interface.
+     */
     MyCom com;
+
+    /**
+     * Represents a Robotino motion drive.
+     */
     OmniDrive omniDrive;
+
+    /**
+     * Array of Robotino motors.
+     */
     Motor motor[3];
+
+    /**
+     * Array of Robotino motors.
+     */
     MotorArray motorArray;
 
 public:
+
+    /**
+     * Constructor. Start connection with Robotino4.
+     * 
+     * @param ip_addr IP-addres of Robotino4.
+     */
     Robotino4(const std::string& ip_addr);
+
+    /**
+     * Destructor. Close connection with Robotino.
+     */
     ~Robotino4();
 
+
+    /**
+     * @return milliseconds since connection to server had ben established.
+     */
     unsigned int get_msec();
+
+    /**
+     * Pause the programm.
+     * 
+     * @param ms pause duration in milliseconds.
+     */
     void sleep(unsigned int ms);
+
+    /**
+     * Encoder position of motor.
+     * 
+     * @param num motor number.
+     * @return actual position of this motor in encoder ticks.
+     */
     int get_actual_position(size_t num);
+
+    /**
+     * Encoder position of all motors.
+     * 
+     * @return actual positionss of all motors in encoder ticks.
+     */
     std::vector<int> get_actual_positions();
+
+    /**
+     * @param num motor number.
+     * @return actual velocity of this motor in rad/s.
+     */
     float get_actual_velocity(size_t num);
+
+    /**
+     * @return actual velocities of all motors in rad/s.
+     */
     std::vector<float> get_actual_velocities();
+
+    /**
+     * @param num motor number.
+     * @return current of this motor in A.
+     */
     float get_actual_current(size_t num);
+
+    /**
+     * @return currents of all motor in A.
+     */
     std::vector<float> get_actual_currents();
+
+    /**
+     * Sets the setpoint speed of this motor.
+     * 
+     * @param num motor number.
+     * @param speed setpoint speed in rad/s.
+     */
     void set_motor_speed(size_t num, float speed);
+
+    /**
+     * Sets the setpoint speed of all motors.
+     * 
+     * @param speed speed setpoints for all motors in rad/s.
+     */
     void set_motors_speed(const std::vector<float>& speeds);
+
+    /**
+     * Resets the position of this motor.
+     * 
+     * @param num motor number.
+     * @param pos new position after reset in encoder ticks.
+     */
     void reset_motor_position(size_t num, int pos);
+
+    /**
+     * Resets the position of all motors.
+     * 
+     * @param pos new positions after reset in encoder ticks.
+     */
     void reset_motors_position(const std::vector<int>& pos);
+
+    /**
+     * Sets the proportional, integral and differential constants of the PID controller.
+     * 
+     * @param num motor number.
+     * @param kp proportional constant of the motor's speed PID controller.
+     * @param ki integral constant of the motor's speed PID controller.
+     * @param kd differential constant of the motor's speed PID controller.
+     * @remark These values are scaled by the microcontroller firmware to match with the PID controller implementation.
+     * If value is given, the microcontroller firmware uses its build in default value.
+     * Robotino v3 Parameters are floating point values used by the microcontroller directly. If parameter is less than 0 the default parameter is used.
+     */
     void set_motor_pid(size_t num, float kp, float ki, float kd);
+
+    /**
+     * Sets the proportional, integral and differential constants of the PID controllers.
+     * 
+     * @param kp proportional constants of the motor' speed PID controllers.
+     * @param ki integral constants of the motor' speed PID controllers.
+     * @param kd differential constants of the motor' speed PID controllers.
+     * @remark These values are scaled by the microcontroller firmware to match with the PID controller implementation.
+     * If value is given, the microcontroller firmware uses its build in default value.
+     * Robotino v3 Parameters are floating point values used by the microcontroller directly. If parameter is less than 0 the default parameter is used.
+     */
     void set_motors_pid(const std::vector<float>& kp, const std::vector<float>& ki, const std::vector<float>& kd);
+
+    /**
+     * Set robot speed.
+     * 
+     * @param vx speed along x axis of robot's local coordinate system in m/s.
+     * @param vy speed along y axis of robot's local coordinate system in m/s.
+     * @param omega angular velocity of rotation in rad/s.
+     * @remark This function is thread save. It should be called about every 100ms.
+     */
     void set_robot_speed(float vx, float vy, float omega);
 
 };
--- src/robotino4.cpp
+++ src/robotino4.cpp
@@ -1,30 +1,25 @@
 #include "robotino4.hpp"
 
-//This function is called on errors.
 void MyCom::errorEvent( const char* errorString )
 {
     std::cerr << "Error: " << errorString << std::endl;
 }
 
-//This function is called if a connection to Robotino has been established.
 void MyCom::connectedEvent()
 {
     std::cout << "Connected." << std::endl;
 }
 
-//This function is called when a connection is closed.
 void MyCom::connectionClosedEvent()
 {
     std::cout << "Connection closed." << std::endl;
 }
 
-//This function is called when a log message is posted.
 void MyCom::logEvent( const char* message, int level )
 {
     std::cout << message << std::endl;
 }
 
-//This function is called when use ping to robot.
 void MyCom::pingEvent( float timeMs )
 {
     std::cout << "Ping: " << timeMs << "ms" << std::endl;
@@ -64,27 +59,20 @@ Robotino4::~Robotino4()
     rec::robotino::api2::shutdown();
 }
 
-//Return milliseconds since connection to server had ben established.
 unsigned int Robotino4::get_msec()
 {
     return com.msecsElapsed();
 }
 
-//Pause the programm for ms milliseconds.
 void Robotino4::sleep(unsigned int ms)
 {
     rec::robotino::api2::msleep(ms);
 }
 
-/*Retrieves the actual position of this motor.
-num - motor number.
-Position in encoder ticks.*/
 int Robotino4::get_actual_position(size_t num){
     return motor[num].actualPosition();
 }
 
-/*Retrieves the actual position of all motors.
-Position in encoder ticks.*/
 std::vector<int> Robotino4::get_actual_positions()
 {
     std::vector<int> pos = {0, 0, 0};
@@ -92,16 +80,11 @@ std::vector<int> Robotino4::get_actual_positions()
     return pos;
 }
 
-/*Retrieves the actual velocity of this motor.
-num - motor number.
-Velocity in rad/s.*/
 float Robotino4::get_actual_velocity(size_t num)
 {
     return 2 * PI * motor[num].actualVelocity() / 60;
 }
 
-/*Retrieves the actual velocity of all motors.
-Velocity in rad/s.*/
 std::vector<float> Robotino4::get_actual_velocities()
 {
     std::vector<float> vel = {0, 0, 0};
@@ -113,16 +96,11 @@ std::vector<float> Robotino4::get_actual_velocities()
     return vel;
 }
 
-/*Retrieves the current of this motor.
-num - motor number.
-Current in A.*/
 float Robotino4::get_actual_current(size_t num)
 {
     return motor[num].motorCurrent();
 }
 
-/*Retrieves the current of all motors.
-Current in A.*/
 std::vector<float> Robotino4::get_actual_currents()
 {
     std::vector<float> cur = {0, 0, 0};
@@ -130,31 +108,21 @@ std::vector<float> Robotino4::get_actual_currents()
     return cur;
 }
 
-/*Sets the setpoint speed of this motor.
-num - motor number.
-speed - setpoint speed in rad/s.*/
 void Robotino4::set_motor_speed(size_t num, float speed)
 {
     motor[num].setSpeedSetPoint(60 * speed / (2 * PI));
 }
 
-/*Sets the setpoint speed of all motors.
-speeds - setpoint speed for all motors in rad/s.*/
 void Robotino4::set_motors_speed(const std::vector<float>& speeds)
 {
     motorArray.setSpeedSetPoints(&speeds[0], motor_num);
 }
 
-/*Resets the position of this motor.
-num - motor number.
-pos - new position after reset in encoder ticks.*/
 void Robotino4::reset_motor_position(size_t num, int pos)
 {
     motor[num].resetPosition(pos);
 }
 
-/*Resets the position of all motors.
-pos - new position after reset in encoder ticks.*/
 void Robotino4::reset_motors_position(const std::vector<int>& pos)
 {
     for (size_t i = 0; i < motor_num; i++)
@@ -164,26 +132,11 @@ void Robotino4::reset_motors_position(const std::vector<int>& pos)
     
 }
 
-/*Sets the proportional, integral and differential constant of the PID controller. 
-num - motor number.
-kp - proportional constant of the motor's speed PID controller.
-ki - integral constant of the motor's speed PID controller.
-kd - differential constant of the motor's speed PID controller.
-These values are scaled by the microcontroller firmware to match with the PID controller implementation.
-If value is given, the microcontroller firmware uses its build in default value.
-Robotino v3 Parameters are floating point values used by the microcontroller directly. If parameter is less than 0 the default parameter is used.*/
 void Robotino4::set_motor_pid(size_t num, float kp, float ki, float kd)
 {
     motor[num].setPID(kp, ki, kd);
 }
 
-/*Sets the proportional, integral and differential constant of the PID controller. 
-kp - proportional constants of the motor's speed PID controllers.
-ki - integral constants of the motor's speed PID controllers.
-kd - differential constants of the motor's speed PID controllers.
-These values are scaled by the microcontroller firmware to match with the PID controller implementation.
-If value is given, the microcontroller firmware uses its build in default value.
-Robotino v3 Parameters are floating point values used by the microcontroller directly. If parameter is less than 0 the default parameter is used.*/
 void Robotino4::set_motors_pid(const std::vector<float>& kp, const std::vector<float>& ki, const std::vector<float>& kd)
 {
     for (size_t i = 0; i < motor_num; i++)
@@ -193,11 +146,6 @@ void Robotino4::set_motors_pid(const std::vector<float>& kp, const std::vector<f
     
 }
 
-/*Set robot speed.
-vx - speed along x axis of robot's local coordinate system in m/s.
-vy - speed along y axis of robot's local coordinate system in m/s.
-omega - angular velocity of rotation in rad/s.
-This function is thread save. It should be called about every 100ms.*/
 void Robotino4::set_robot_speed(float vx, float vy, float omega)
 {
     omniDrive.setVelocity(vx, vy, omega);
