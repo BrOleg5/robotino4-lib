be798ef2afced9126004dc843e0d21248bb42c7a
BrOleg5 <oleg.brylyov@mail.ru>
2022-05-28 15:40:15 +0700

    Add in example program command line options.

--- examples/move_forward.cpp
+++ examples/move_forward.cpp
@@ -1,16 +1,37 @@
 #include "robotino4.hpp"
+#include "inputparser.hpp"
+#include <iostream>
+#include <iomanip>
 
 int main( int argc, char **argv )
 {
-    //Default IP addres Robotino 4
+    // Default IP addres Robotino 4
     std::string ip_addr = "192.168.0.1";
-    if (argc > 1)
-    {
-        ip_addr = argv[1];
+
+    unsigned int test_duration = 10000;
+
+    // Process command line options
+    if(argc > 1) {
+        InputParser input(argc, argv);
+        std::streamsize st_size = 25;
+        // Help option
+        if(input.cmdOptionExists("-h") || input.cmdOptionExists("-?") || input.cmdOptionExists("--help")) {
+            std::cout << "usage: move_forward [options]\n\nOptions:\n"
+                        << std::left << "  " << std::setw(st_size) << "-h/-?/--help" << "Display this information.\n"
+                        << "  " << std::setw(st_size) << "-ip <address>" << "Set Robotino4 IP-<address>.\n"
+                        << "  " << std::setw(st_size) << "-t <time>" << "Set duration of execution. <time> in ms\n";
+                exit(0);
+        }
+        if(input.cmdOptionExists("-ip"))
+        {
+            ip_addr = input.getCmdOption("-ip");
+        }
+        if(input.cmdOptionExists("-t")) {
+            test_duration = std::stoi(input.getCmdOption("-t"));
+        }
     }
     Robotino4 robotino(ip_addr);
     
-    unsigned int test_duration = 10000;
     unsigned int time = robotino.get_msec();
     std::cout << "| Time | M1 pos | M2 pos | M3 pos | M1 vel | M2 vel | M3 vel| M1 cur | M2 cur | M3 cur |" << std::endl;
     std::cout << "|--------------------------------------------------------------------------------------|" << std::endl;
